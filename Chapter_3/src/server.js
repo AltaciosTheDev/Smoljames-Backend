da96e747fcbf91bfc248aecb2cfee5e34a93a08c
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-12-22 18:57:01 -0600

    added public files

--- Chapter_3/src/server.js
+++ Chapter_3/src/server.js
@@ -1,8 +1,11 @@
-const express = require('express')
+import express from 'express' // Import express from the express package
+
+
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
-app.listen(3000, (() => {
-    console.log('Server is running on port 3000')
+app.listen(PORT, (() => {
+    console.log(`Server is running on port: ${PORT}`)
 }))
